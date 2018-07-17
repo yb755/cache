278d2c23cbb37834d6129881975c4625a6f5dd4b
yebing <yebing@vvsmark.com>
2018-07-17 21:01:14 +0800

    update

--- cache-sample/src/main/java/com/vbgps/cache/simple/ehcache/Main.java
+++ cache-sample/src/main/java/com/vbgps/cache/simple/ehcache/Main.java
@@ -1,17 +1,24 @@
 package com.vbgps.cache.simple.ehcache;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext factory = new ClassPathXmlApplicationContext("classpath:spring-*.xml");
+		final AbstractApplicationContext  factory = new ClassPathXmlApplicationContext("classpath:spring-*.xml");
 		UserService userService = factory.getBean(UserService.class);
 		UserInfo user = userService.findById(22);
 		System.out.println(user.getUserName());
 		UserInfo user2 = userService.findById(22);
 		System.out.println(user2.getUserName());
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+			public void run() {
+				System.out.println("应用关闭");
+				factory.close();
+			}
+		}));
 	}
 
 }
